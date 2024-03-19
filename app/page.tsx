68541e5417df26004e2fc7ac84c9e9b334dc4c4c
Daniel R. Muller <125689306+Daniel-Muller45@users.noreply.github.com>
2024-03-19 19:18:43 -0400

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,125 @@
-"use client"
+import Microphone from './assets/Microphone.png'
+import Artificial from './assets/Artificial.png'
+import Mockup from './assets/Mockup.png'
+import Watch from './assets/Watch.png'
+import Protect from './assets/Protect.png'
+import Image from 'next/image'
 
-import React, {useState, useContext, useEffect} from 'react';
-import { Button } from "@/components/ui/button"
-import { Textarea } from '@/components/ui/textarea'
-import { Label } from "@/components/ui/label"
-import {fetchLogs, postLog} from "./utils/api"
-import { createClient } from './utils/supabase/client'
-
-
-interface User {
-    id: string;
-}
-
-export default function Page() {
-    const [user, setUser] = useState<User | null>(null);
-    const [mealDescription, setMealDescription] = useState('');
-
-    useEffect(() => {
-        (async () => {
-            const supabase = createClient();
-            const { data: userData } = await supabase.auth.getUser();
-            setUser(userData.user); // Assuming userData has a user object
-        })();
-    }, []);
-
-    const handleMealSubmit = async () => {
-        if (!mealDescription.trim()) {
-            alert('Please enter a description for the meal.');
-            return;
-        }
-        if (!user || !user.id) {
-            alert('You must be logged in to log a meal.');
-            return;
-        }
-
-        try {
-            const result = await postLog(mealDescription, user.id);
-            if (result.error) {
-                alert(`Error: ${result.error}`);
-            } else {
-                alert('Meal logged successfully');
-                setMealDescription('');
-            }
-        } catch (error) {
-            console.error('Error logging meal:', error);
-            alert('Failed to log meal. Please try again.');
-        }
-    };
-
+export default function Main() {
     return (
-        <div className="grid gap-1.5 items-center w-1/2 mx-auto mt-44">
-            <Label htmlFor="message-2">Log Your Meals and Workouts</Label>
-            <Textarea
-                placeholder="Type your message here."
-                id="message-2"
-                className="mt-5"
-                value={mealDescription}
-                onChange={(e) => setMealDescription(e.target.value)}
-            />
-            <Button className="mt-3" onClick={handleMealSubmit}>Log Meal</Button>
-        </div>
+        <section className="text-gray-600 body-font">
+            <div className="max-w-7xl mx-auto flex px-5 py-12 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
+                    <h1 className="mb-5 sm:text-6xl text-5xl items-center Avenir xl:w-2/2 text-white font-semibold">
+                        Log your Meals Faster than Ever
+                    </h1>
+                    <p className="mb-4 xl:w-3/4 text-gray-400 text-lg">
+                        Use natural language to log your meals in seconds. Get on the fly nutrition recommendations.
+                    </p>
+                    <div className="flex justify-center">
+                        <a
+                            className="inline-flex items-center px-5 py-3 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg"
+                            href="/login"
+                        >
+                            <span className="justify-center">Try out the beta</span>
+                        </a>
+                    </div>
+                </div>
+                <div className="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
+                    <Image
+                        className="w-80 md:ml-1 ml-24"
+                        alt="iPhone-12"
+                        src={Mockup}
+                    ></Image>
+                </div>
+            </div>
+            <div className="grr max-w-7xl pt-20 mx-auto text-center">
+                <h1 className="mb-8 text-6xl Avenir font-semibold text-white">
+                    Less time, less effort.
+                </h1>
+                <h1 className="mb-8 text-2xl Avenir font-semibold text-gray-400 text-center">
+                    Let us do the heavy lifting. Well, at least outside of the gym.
+                </h1>
+            </div>
+            <section>
+                <div className="container mx-auto p-4">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+                        <div className="bg-gray-900 rounded-lg shadow overflow-hidden">
+                            <div className="flex justify-center items-center h-48 sm:h-64 bg-gray-900">
+                                <Image src={Artificial} alt='brain'
+                                />
+                            </div>
+                            <div className="p-4">
+                                <h2 className="font-bold text-xl mb-2 text-white">Personalized Recommendations</h2>
+                                <p className="text-gray-400 text-base">
+                                    Get nutrition advice based on your tendencies and goals.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="bg-gray-900 rounded-lg shadow overflow-hidden">
+                            <div className="flex justify-center items-center h-48 sm:h-64 bg-gray-900">
+                                <Image src={Watch} alt='brain'
+                                />
+                            </div>
+                            <div className="p-4">
+                                <h2 className="font-bold text-xl mb-2 text-white">Integrate with Wearables</h2>
+                                <p className="text-gray-400 text-base">
+                                    Sync your wearables to track key fitness metrics.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="bg-gray-900 rounded-lg shadow overflow-hidden">
+                            <div className="flex justify-center items-center h-48 sm:h-64 bg-gray-900">
+                                <Image src={Microphone} alt='brain'
+                                />
+                            </div>
+                            <div className="p-4">
+                                <h2 className="font-bold text-xl mb-2 text-white">Effortless Fitness Tracking</h2>
+                                <p className="text-gray-400 text-base">
+                                    Use siri to log your meals without even opening your phone.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="bg-gray-900 rounded-lg shadow overflow-hidden">
+                            <div className="flex justify-center items-center h-48 sm:h-64 bg-gray-900">
+                            <Image src={Protect} alt='brain'
+                                />
+                            </div>
+                            <div className="p-4">
+                                <h2 className="font-bold text-xl mb-2 text-white">Accurate and Secure</h2>
+                                <p className="text-gray-400 text-base">
+                                    Get the most up to date nutrition data, while keeping your fitness data secure.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="relative">
+                <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
+                <div className="py-24 md:py-36">
+                        <h1 className="mb-5 text-6xl Avenir font-semibold text-white">
+                            Subscribe to our newsletter
+                        </h1>
+                        <h1 className="mb-9 text-2xl font-semibold text-gray-400">
+                            Get private access to our launch.
+                        </h1>
+                        <input
+                            placeholder="jack@example.com"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            className="border border-gray-600 w-1/4 pr-2 pl-2 py-3 mt-2 rounded-md text-gray-800 font-semibold hover:border-gray-900"
+                        ></input>{" "}
+                        <a
+                            className="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg"
+                            href="/"
+                        >
+                            <span className="justify-center">Subscribe</span>
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </section>
     );
 }
